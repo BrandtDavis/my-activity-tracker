7f048fd473c4332bb7177b25bbf146b5adfeabb4
Brandt Davis <brandtd77@gmail.com>
2023-03-03 07:55:02 -0700

    Update to get all activities

--- main.py
+++ main.py
@@ -10,26 +10,18 @@ from pprint import pprint
 
 load_dotenv()
 
-authUrl = 'https://www.strava.com/api/v3/athlete'
+activitiesUrl = 'https://www.strava.com/api/v3/athlete/activities'
 
 token = os.getenv("ACCESS_TOKEN")
-headers = {"Authorization": f"Bearer {token}"}
+# headers = {"Authorization": f"Bearer {token}"}
 
-response = requests.get(authUrl, headers=headers)
+params = {
+    'access_token': os.getenv("ACCESS_TOKEN")
+}
+
+response = requests.get(activitiesUrl, params=params)
 json_response = response.json()
 
-# Configure OAuth2 access token for authorization: strava_oauth
-swagger_client.configuration.access_token = token
+pprint(json_response)
 
-# create an instance of the API class
-api_instance = swagger_client.ActivitiesApi()
-id = 0 # Long | The identifier of the activity.
-includeAllEfforts = True # Boolean | To include all segments efforts. (optional)
-
-try: 
-    # Get Activity
-    api_response = api_instance.getActivityById(id, includeAllEfforts=includeAllEfforts)
-    pprint(api_response)
-except ApiException as e:
-    print("Exception when calling ActivitiesApi->getActivityById: %s\n" % e)
 
