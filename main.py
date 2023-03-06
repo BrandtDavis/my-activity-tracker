db9b4a5667831c52f23513433737afe252a9e868
Brandt Davis <brandtd77@gmail.com>
2023-03-06 08:37:46 -0700

    Add logic to automatically refresh token

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from dotenv import load_dotenv
 
 from pprint import pprint
 
+from authorization import refresh_user_access
+
 load_dotenv()
 
 activitiesUrl = 'https://www.strava.com/api/v3/athlete/activities'
@@ -15,9 +17,24 @@ token = os.getenv("ACCESS_TOKEN")
 params = {
     'access_token': os.getenv("ACCESS_TOKEN")
 }
-
 response = requests.get(activitiesUrl, params=params)
 json_response = response.json()
+
+if json_response['code'] == 'invalid' and json_response['field'] == 'access_token':
+    print("Invalid token, attempting refresh...")
+    originalToken = os.getenv("ACCESS_TOKEN")
+
+    # refresh access token
+    response = refresh_user_access()
+    newToken = os.getenv("ACCESS_TOKEN")
+
+    
+    # if valid, carry on, else exit gracefully
+    if(originalToken != newToken):
+        print("Token updated successfully")
+    else:
+        print("Could not create new token, exitting") 
+
 
 pprint(json_response)
 
