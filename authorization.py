312845e2772e8ce60763f2fec2ffb1417ce3dab4
Brandt Davis <brandtd77@gmail.com>
2023-03-05 22:51:46 -0700

    Add function to get refresh access token

--- authorization.py
+++ authorization.py
@@ -22,8 +22,31 @@ def authorize_user():
     # headers = {"Authorization": f"Bearer {token}"}
 
     response = requests.post(authUrl, params=params)
-    json_response = response.json()
-    return json_response
+    return response.json()
 
-response = authorize_user()
-pprint(response)
+def refresh_user_access():
+    load_dotenv()
+
+    authUrl = 'https://www.strava.com/oauth/token'
+
+    clientId = os.getenv("CLIENT_ID")
+    clientSecret = os.getenv("CLIENT_SECRET")
+    refreshToken = os.getenv("REFRESH_TOKEN")
+
+    params = {
+        'client_id': clientId,
+        'client_secret': clientSecret,
+        'refresh_token': refreshToken,
+        'grant_type': 'refresh_token'
+    }
+
+    response = requests.post(authUrl, params=params)
+    
+    # ** Investigate why this doesn't work **
+    # json_response = response.json()
+    pprint(response.__dict__)
+    return response.json()
+
+response = refresh_user_access()
+#pprint(response)
+
