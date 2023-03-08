49b6315b3358153492dedd86fc3f70986ac4849b
Brandt Davis <brandtd77@gmail.com>
2023-03-07 23:34:37 -0700

    Extract get all activities

---
+++ activities.py
@@ -0,0 +1,23 @@
+# from __future__ import print_statement
+import requests
+import os
+from dotenv import load_dotenv
+
+from pprint import pprint
+
+from authorization import refresh_user_access
+
+def getAllUserActivites():
+    load_dotenv()
+
+    activitiesUrl = 'https://www.strava.com/api/v3/athlete/activities'
+
+    token = os.getenv("ACCESS_TOKEN")
+
+    params = {
+        'access_token': os.getenv("ACCESS_TOKEN")
+    }
+    response = requests.get(activitiesUrl, params=params)
+    return response.json()
+
+
