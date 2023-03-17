908e8c5dded793a4415e76e3702be3c9cde00f5e
Brandt Davis <brandtd77@gmail.com>
2023-03-16 22:59:38 -0600

    Update function to create array of all activities

--- Athlete.py
+++ Athlete.py
@@ -7,14 +7,26 @@ from dotenv import load_dotenv
 
 def getAllUserActivites():
     load_dotenv()
-
+    responses = []
     activitiesUrl = 'https://www.strava.com/api/v3/athlete/activities'
 
-    params = {
-        'access_token': os.getenv("ACCESS_TOKEN")
-    }
-    response = requests.get(activitiesUrl, params=params).json()
-    return response
+    pageNum = 1
+
+    while(1):
+        params = {
+            'access_token': os.getenv("ACCESS_TOKEN"),
+            'per_page': 200,
+            'page': pageNum
+        }
+
+        activities = requests.get(activitiesUrl, params=params).json()
+        responses = responses + activities
+        
+        pageNum+=1
+
+        if len(activities) < 200: break
+
+    return responses
 
 
 
