fd6336c63ba0d21bbad77adec05e1232b2f33a81
Brandt Davis <brandtd77@gmail.com>
2023-03-25 16:06:39 -0600

    Automate token refresh and improve error handling

--- Authorization.py
+++ Authorization.py
@@ -4,58 +4,79 @@ import dotenv
 from dotenv import load_dotenv
 from pprint import pprint
 
-def authorize_user():
-    load_dotenv()
+class Authorization:
 
-    authUrl = 'https://www.strava.com/oauth/token'
+    def __init__(self):
+        pass
 
-    clientId = os.getenv("CLIENT_ID")
-    clientSecret = os.getenv("CLIENT_SECRET")
-    authCode = os.getenv("AUTHORIZATION_CODE")
+    def authorize_user(self):
+        load_dotenv()
 
-    params = {
+        authUrl = 'https://www.strava.com/oauth/token'
+
+        clientId = os.getenv("CLIENT_ID")
+        clientSecret = os.getenv("CLIENT_SECRET")
+        authCode = os.getenv("AUTHORIZATION_CODE")
+
+        params = {
+            'client_id': clientId,
+            'client_secret': clientSecret,
+            'code': authCode,
+            'grant_type': 'authorization_code'
+        }
+
+        # {'message': 'Bad Request', 'errors': [{'resource': 'AuthorizationCode', 'field': 'code', 'code': 'invalid'}]}
+        try:
+            response = requests.post(authUrl, params=params).json()
+            if response['errors']:
+                errorList = response['errors']
+                if errorList[0]['resource'] == "AuthorizationCode" and errorList[0]['code'] == 'invalid':
+                    print("Invalid Auth code, attempting refresh...")
+                    response = self.refresh_user_access()
+                else:
+                    print("Unknown error :(")
+
+            return response
+        
+        except requests.exceptions.ConnectionError:
+            return 'Connection Error'
+
+
+
+    def get_new_access_token(self):
+        load_dotenv()
+
+        authUrl = 'https://www.strava.com/oauth/token'
+
+        clientId = os.getenv("CLIENT_ID")
+        clientSecret = os.getenv("CLIENT_SECRET")
+        refreshToken = os.getenv("REFRESH_TOKEN")
+
+        params = {
         'client_id': clientId,
         'client_secret': clientSecret,
-        'code': authCode,
-        'grant_type': 'authorization_code'
-    }
+        'refresh_token': refreshToken,
+        'grant_type': 'refresh_token'
+        }
 
-    response = requests.post(authUrl, params=params)
-    return response.json()
-
-
-def get_new_access_token():
-    load_dotenv()
-
-    authUrl = 'https://www.strava.com/oauth/token'
-
-    clientId = os.getenv("CLIENT_ID")
-    clientSecret = os.getenv("CLIENT_SECRET")
-    refreshToken = os.getenv("REFRESH_TOKEN")
-
-    params = {
-    'client_id': clientId,
-    'client_secret': clientSecret,
-    'refresh_token': refreshToken,
-    'grant_type': 'refresh_token'
-    }
-
-    response = requests.post(authUrl, params=params)
-    
-    # ** Investigate why this doesn't work **
-    # json_response = response.json()
-    # pprint(response.__dict__)
-    return response.json()['access_token']
+        try:
+            response = requests.post(authUrl, params=params)
+            return response.json()['access_token']
+        except ConnectionRefusedError:
+            return 'Connection Refused'
+        # ** Investigate why this doesn't work **
+        # json_response = response.json()
+        # pprint(response.__dict__)
 
 
-def refresh_user_access():
-    load_dotenv()
-    dotenvFile = dotenv.find_dotenv()
+    def refresh_user_access(self):
+        load_dotenv()
+        dotenvFile = dotenv.find_dotenv()
 
-    newAccessToken = get_new_access_token()
-    dotenv.set_key(dotenvFile, "ACCESS_TOKEN", newAccessToken)
+        newAccessToken = self.get_new_access_token()
+        dotenv.set_key(dotenvFile, "ACCESS_TOKEN", newAccessToken)
 
 
-refresh_user_access()
+    # refresh_user_access()
 
 
