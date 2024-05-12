c6cceb747cf0b1e95b423258e3f4072fa935f00b
Brandt Davis <brandtd77@gmail.com>
2024-05-11 22:54:39 -0600

    Add basic DB connection

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from dotenv import load_dotenv
 
 from pprint import pprint
 
+import pymongo
+from pymongo import MongoClient
+
 from Authorization import Authorization
 from Athlete import getAllUserActivites, getAllRunBasedActivities
 
@@ -13,6 +16,23 @@ activities = getAllUserActivites()
 
 print(len(activities))
 
+
+try:
+    uri = os.getenv("DB_CONN_STRING")
+    client = MongoClient(uri)
+
+    database = client[os.getenv("DB_NAME")]
+    collection = database[os.getenv("DB_COLLECTION")]
+
+    collection_list = database.list_collections()
+    if "activities" not in collection_list:
+        database.create_collection("activities")
+
+    client.close()
+
+except Exception as e:
+    raise Exception(
+        "The following error occurred: ", e)
 
 
 # pprint(activities[3]['type'])
