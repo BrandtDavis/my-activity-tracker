9fc8a1a21e7e3bdd396aae12fb283dfd8f401da0
Brandt Davis <brandtd77@gmail.com>
2024-05-13 23:15:41 -0600

    Refactor to use fields, add comment

--- database/Connection.py
+++ database/Connection.py
@@ -1,29 +1,34 @@
 import os
+from dotenv import load_dotenv
 from pymongo import MongoClient
 
 class Connection:
     def __init__(self):
+        load_dotenv()
         self.DB_CONN_STRING = os.getenv("DB_CONN_STRING")
         self.DB_NAME = os.getenv("DB_NAME")
         self.DB_COLLECTION = os.getenv("DB_COLLECTION")
 
-        self.connect()
+        # self.conn = self.connect()
 
     def connect(self):
+        print(self.DB_CONN_STRING)
         try:
-            uri = os.getenv("DB_CONN_STRING")
+            uri = self.DB_CONN_STRING
             client = MongoClient(uri)
 
-            database = client[os.getenv("DB_NAME")]
-            collection = database[os.getenv("DB_COLLECTION")]
+            database = client[self.DB_NAME]
+            collection = database[self.DB_COLLECTION]
 
+            # Extract this into a DB initialization script
             collection_list = database.list_collection_names()
             if "activities" not in collection_list:
                 print("Creating collection with name 'activities'")
                 database.create_collection("activities")
             
 
-            client.close()
+            # client.close()
+            return database
 
         except Exception as e:
             raise Exception(
