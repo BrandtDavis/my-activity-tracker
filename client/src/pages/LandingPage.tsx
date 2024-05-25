56afd395505a2d6eab24c53e8064333d42050376
Brandt Davis <brandtd77@gmail.com>
2024-05-25 12:45:20 -0600

    Add axios, create separate login page

--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -1,39 +1,11 @@
 import React from "react";
 
+
+
 export const LandingPage = () => {
     return (
         <div>
-            
-            
             <div className="w-full max-w-md mx-auto mt-20">
-            <form className="bg-white shadow-lg rounded px-8 pt-6 pb-8 mb-4">
-                <h2 className="flex justify-center mb-4 text-xl font-bold">Strava dashboard</h2>
-
-                <div className="mb-4">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
-                        username
-                    </label>
-                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="username" />
-                </div>
-                <div className="mb-6">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
-                        password
-                    </label>
-                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************"/>
-                    {/* <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************"/> */}
-                    {/* <p className="text-red-500 text-xs italic">Please choose a password.</p> */}
-                </div>
-                <div className="flex items-center justify-between">
-                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
-                        Sign In
-                    </button>
-                    <a className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
-                        Forgot Password?
-                    </a>
-                </div>
-            </form>
-            <p className="text-center text-gray-500 text-xs">
-            </p>
             </div>
         </div>
     );
