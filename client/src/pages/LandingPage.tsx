ed22cc697b7592fc9f7fd19505e640c5ffc1b68d
Brandt Davis <brandtd77@gmail.com>
2024-05-25 11:49:36 -0600

    Update styling

--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -5,8 +5,8 @@ export const LandingPage = () => {
         <div>
             
             
-            <div className="w-full max-w-md my-auto mx-auto">
-            <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            <div className="w-full max-w-md mx-auto mt-20">
+            <form className="bg-white shadow-lg rounded px-8 pt-6 pb-8 mb-4">
                 <h2 className="flex justify-center mb-4 text-xl font-bold">Strava dashboard</h2>
 
                 <div className="mb-4">
@@ -19,8 +19,9 @@ export const LandingPage = () => {
                     <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
                         password
                     </label>
-                    <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************"/>
-                    <p className="text-red-500 text-xs italic">Please choose a password.</p>
+                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************"/>
+                    {/* <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************"/> */}
+                    {/* <p className="text-red-500 text-xs italic">Please choose a password.</p> */}
                 </div>
                 <div className="flex items-center justify-between">
                     <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
