797486f22138795c08f8586aafbbbca12f657d47
Brandt Davis <brandtd77@gmail.com>
2024-06-06 10:41:40 -0600

    Update styling, add profile page to routes

--- client/src/pages/ProfilePage.tsx
+++ client/src/pages/ProfilePage.tsx
@@ -1,29 +1,32 @@
 export const ProfilePage = () => {
     let firstName = "Brandt"
-    let lastName = "davis"
+    let lastName = "Davis"
     let email = "Brandt@test.com"
 
     return (
         <>
             <h1 className="flex ml-4 mt-4 text-xl">Profile</h1>
-            <div className="border-2 mx-auto max-w-screen-md">
+            <div className="border mx-auto rounded shadow-md max-w-screen-md">
                 {/* Why Does Label Need to be First? */}
 
-                <div className="block mt-2">
+                <div className="block ml-4 mt-2">
                     <label className="float" htmlFor="first-name">First Name:</label>
-                    <input className="shadow float" readOnly={true} id="first-name" value={firstName}></input>
+                    <input className="shadow float ml-2 pl-2" readOnly={true} id="first-name" value={firstName}></input>
                 </div>
 
-                <div className="block mt-2">
-                    <label className="float" htmlFor="first-name">Last Name:</label>
-                    <input className="text-l shadow" type="text" readOnly={true} id="last-name" value={lastName}></input>
+                <div className="block ml-4 mt-2">
+                    <label className="float" htmlFor="last-name">Last Name:</label>
+                    <input className="text-l shadow ml-2 pl-2" type="text" readOnly={true} id="last-name" value={lastName}></input>
                 </div>
 
-                <div className="block mt-2">
-                    <label className="float" htmlFor="first-name">Email:</label>
-                    <input className="text-l shadow" type="email" readOnly={true} id="email" value={email}></input>
+                <div className="block ml-4 mt-2">
+                    <label className="float" htmlFor="email">Email:</label>
+                    <input className="text-l shadow ml-2 pl-2" type="email" readOnly={true} id="email" value={email}></input>
                 </div>
 
+                <div className="block max-w-lg ml-12 mt-4 mb-4">
+                    <input className="mx-auto px-4 py-1 rounded border-2 hover:cursoer" type="button" value="Back" />
+                </div>
             </div>
         </>
     )
