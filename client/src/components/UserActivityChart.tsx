ba6206bad9c2d19dd94c9dafbd2f3b228e9777b3
Brandt Davis <brandtd77@gmail.com>
2024-06-23 09:37:37 -0600

    Add ability to get activity data

--- client/src/components/UserActivityChart.tsx
+++ client/src/components/UserActivityChart.tsx
@@ -1,17 +1,44 @@
 import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-export const UserActivityChart = (activityData: any) => {
+export const UserActivityChart = (athleteId: any) => {
+  const [activityData, setActivityData] = useState(
+    [
+      {
+        date: '',
+        distance: 0,
+      }
+    ]
+  );
 
-  const data = [
-    {date: 'Page A', uv: 400, amt: 5},
-    {date: 'Page B', uv: 100, pv: 2400},
-    {date: 'Page C', uv: 250, pv: 2400, amt: 2400},
-    {date: 'Page C', uv: 250, pv: 2400, amt: 2400},
-  ];
-  
+  const url = `http://127.0.0.1:5000/athlete_activities?athlete_id=${athleteId.athleteId}`
+
+  useEffect(() => {
+    axios.get(url, {
+      headers: {
+        'Content-type':'application/json', 
+        'Accept':'application/json'
+      },
+    })
+    .then((response) => {
+      console.log(response)
+      setActivityData(response.data.activities)
+      console.log("ACTIVITIES: ", activityData)
+    });
+  }, []);
+
+  // Placeholder data - For testing formatting, etc.
+  // const data = [
+  //   {date: 'Page A', distance: 400, amt: 5},
+  //   {date: 'Page B', distance: 100, pv: 2400},
+  //   {date: 'Page C', distance: 250, pv: 2400, amt: 2400},
+  //   {date: 'Page C', distance: 250, pv: 2400, amt: 2400},
+  // ];
+
     const renderLineChart = (
-        <LineChart width={600} height={300} data={data}>
-          <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+        <LineChart width={600} height={300} data={activityData}>
+          <Line type="monotone" dataKey="distance" stroke="#8884d8" />
           <CartesianGrid stroke="#ccc" />
           <XAxis dataKey="date" />
           <YAxis />
