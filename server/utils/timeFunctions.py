f68d1b78db8ccb836adf56e87595d40f38cd66ac
Brandt Davis <brandtd77@gmail.com>
2024-07-03 07:26:47 -0600

    Add placeholder for function to return array of dates

---
+++ server/utils/timeFunctions.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+
+# get the past x number of weeks
+# Returns an array of dates in the format: DD-MM-YYYY
+
+# Help: https://stackoverflow.com/questions/9847213/how-do-i-get-the-day-of-week-given-a-date
+def get_last_x_weeks(x: int):
+    now = datetime.now()
+    print(now.today().weekday())
+
+get_last_x_weeks(3)
