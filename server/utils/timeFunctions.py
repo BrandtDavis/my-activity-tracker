ef42be514cee73289755ee9502397021ee0f9728
Brandt Davis <brandtd77@gmail.com>
2024-07-16 22:26:17 -0600

    Update timeFunctions

--- server/utils/timeFunctions.py
+++ server/utils/timeFunctions.py
@@ -4,11 +4,7 @@ from datetime import datetime
 # Returns an array of dates in the format: DD-MM-YYYY
 
 # Help: https://stackoverflow.com/questions/9847213/how-do-i-get-the-day-of-week-given-a-date
-<<<<<<< HEAD
 def get_last_x_weeks(x: int):
-=======
-def get_last_x_weeks(x: int, activityList):
->>>>>>> a83e715 (Add time timeFunctions)
     now = datetime.now()
     print(now.today().weekday())
 
