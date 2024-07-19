4a8272909db9fb768950d11856cf9ecbe7bebaf9
Brandt Davis <brandtd77@gmail.com>
2024-07-19 15:41:03 -0600

    Add ability to get all Monday dates between the first and most recent activities

--- server/utils/timeFunctions.py
+++ server/utils/timeFunctions.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from datetime import datetime
+from datetime import datetime, timedelta
 from database.AthleteQueries import get_athlete_activities
 
 # Want: 
@@ -23,16 +23,42 @@ def get_activity_list_timeframe(activityList):
 # - Be a Monday
 # - Include the timeframe of the dates returned by get_activity_list_timeframe
 #   - i.e., The Monday for the week of each of the 2 dates returned from the function should be included in the output of get_date_windows
-def get_date_windows():
-    pass
+# Help: https://stackoverflow.com/questions/9847213/how-do-i-get-the-day-of-week-given-a-date
+def get_date_windows(start_date, end_date):
+    start_date_weekday = get_weekday(start_date)
+    end_date_weekday = get_weekday(end_date)
+
+    start_date_offset = start_date_weekday - 1
+    end_date_offset = end_date_weekday - 1
+
+    start_date_window = get_date_object(start_date) - timedelta(days=start_date_offset)
+    end_date_window = get_date_object(end_date) - timedelta(days=end_date_offset)
+
+    date_windows = []
+    date_window = start_date_window
+    while date_window != end_date_window:
+        date_windows.append(date_window.strftime('%Y-%m-%d'))
+        date_window = date_window + timedelta(days=7)
+
+    return date_windows
+
+# Expects a string in the form of "YYYY-mm-dd"
+def get_date_object(date_string):
+    return datetime.strptime(date_string, '%Y-%m-%d').date()
+
+# Expects a date in the form of "YYYY-mm-dd"
+def get_weekday(date):
+    return get_date_object(date).isoweekday()
 
 def group_activities_by_week():
     pass
 
-# Help: https://stackoverflow.com/questions/9847213/how-do-i-get-the-day-of-week-given-a-date
+
 activities = get_athlete_activities(95573308)
 
 timeframe = get_activity_list_timeframe(activities)
 print(f'\nTimeframe: {timeframe}\n')
 
+date_windows = get_date_windows(timeframe[0], timeframe[1])
+print(date_windows)
 # group_activities_by_week(activities)
