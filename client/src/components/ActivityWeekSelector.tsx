0c2bd974b917eba45342d4d2e1e454436f1877d3
Brandt Davis <brandtd77@gmail.com>
2024-08-23 19:32:25 -0600

    Update wording

--- client/src/components/ActivityWeekSelector.tsx
+++ client/src/components/ActivityWeekSelector.tsx
@@ -6,14 +6,14 @@ export class ActivityWeekSelector extends Component {
         return (
             <>
                 <div className="inline-block">
-                    <input type="radio" id="2-week-radio-btn" value="" />
-                    <label htmlFor="2-week-radio-btn">2 Weeks</label>
+                    <input type="radio" id="2-week-radio-btn" value="" className="appearance-none"/>
+                    <label htmlFor="2-week-radio-btn" className="border-2 px-2">2 Weeks</label>
 
-                    <input type="radio" id="4-week-radio-btn" />
-                    <label htmlFor="4-week-radio-btn">4 Weeks</label>
+                    <input type="radio" id="4-week-radio-btn" className="appearance-none" />
+                    <label htmlFor="4-week-radio-btn" className="border-2 px-2">4 Weeks</label>
 
-                    <input type="radio" id="8-week-radio-btn" />
-                    <label htmlFor="8-week-radio-btn">8 Weeks</label>
+                    <input type="radio" id="8-week-radio-btn" className="appearance-none" />
+                    <label htmlFor="8-week-radio-btn"className="border-2 px-2">8 Weeks</label>
                 </div>
             </>
         )
