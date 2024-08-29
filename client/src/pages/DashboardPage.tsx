883938234f27a06dbaf10c71084141afd82b04f6
Brandt Davis <brandtd77@gmail.com>
2024-08-28 22:33:15 -0600

    Refactor to generate list of ActivityStatCard components

--- client/src/pages/DashboardPage.tsx
+++ client/src/pages/DashboardPage.tsx
@@ -3,6 +3,12 @@ import { useParams } from "react-router-dom"
 import { useEffect, useState } from "react";
 
 import { ActivityStatCard } from "../components/ActivityStatCard";
+interface ActivityStatCardProps {
+    header: string
+    body: string
+    data?: number
+    unit?: string
+}
 
 export const DashboardPage = () => {
 
@@ -23,32 +29,48 @@ export const DashboardPage = () => {
       }); 
     }, []);
     
+
+    let activityCardGridData: ActivityStatCardProps[] = [
+        {
+            header: "Total Hours of Activity",
+            body: "This is the total time you've spent doing activities.",
+            data: 100,
+            unit: "Hours"
+        },
+        {
+            header: "Total Number of Activities",
+            body: "This is the total number of activities you have recorded.",
+            data: 0,
+            unit: ""
+        },
+        {
+            header: "Total KMs Run",
+            body: "This is the total KMs run or trail run.",
+            data: undefined,
+            unit: ""
+        },
+        {
+            header: "Total KMs Biked",
+            body: "This is the total KMs biked.",
+            data: undefined,
+            unit: ""
+        }
+    ]
+
     return (
         <div className="h-100">
             <h1 className="pl-4">{athleteName}'s Dashboard</h1>
-            <div className="container mx-auto border-2 px-4 py-4">
-
-                <ActivityStatCard 
-                  header="Total Hours of Activity"
-                  body="This is the total time you've spent doing activities."
-                  data={100}
-                  unit={"Hours"}
-                />
-
-                <ActivityStatCard 
-                  header="Total Number of Activities"
-                  body="This is the total number of activities you have recorded."
-                />
-
-                <ActivityStatCard 
-                  header="Total KMs Run"
-                  body="This is the total KMs run or trail run."
-                />
-
-                <ActivityStatCard 
-                  header="Total KMs"
-                  body="This is the total KMs biked."
-                />
+            <div className="flex flex-row gap-4 flex-wrap mx-auto border-2">
+                {
+                    activityCardGridData.map((item) => (
+                        <ActivityStatCard 
+                            header={item.header}
+                            body={item.body}
+                            data={item.data}
+                            unit={item.unit}
+                        />
+                    ))
+                }
             </div>
         </div>
     );
